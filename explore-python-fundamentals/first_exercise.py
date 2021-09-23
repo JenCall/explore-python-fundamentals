9c7a453955566b5e0adf70c984471149fbcfb8d8
Jeniffer Callou <jenifferprc@gmail.com>
2021-09-23 21:11:08 +0200

    added first file exercise

---
+++ explore-python-fundamentals/first_exercise.py
@@ -0,0 +1,43 @@
+# Variables
+# Explore the Problem with Immutable vs. Mutable Objects
+
+# integer
+a = 8
+b = a
+b = 5
+print(a,b)
+
+# float
+c = 9.5
+d = c
+d = 10.5
+print(c,d)
+
+# str
+f = "Hello"
+g = f
+g = "World"
+print(f,g)
+
+# list
+h = [1,2,3]
+i = h
+i[0] = 4
+print(h,i)
+
+# tuple
+j = ("apple", "banana", "cherry")
+k = j
+k[0] = "kiwi"
+print(j,k)
+
+# dict
+l = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+m = l
+m[0] = {"hello":"world"}
+print(l,m)
